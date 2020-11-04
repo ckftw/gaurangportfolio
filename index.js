8b1f1847eb5eb1f813f9855c065e2251b2903abb
Gaurang Shroff <shroffgaurang9@gmail.com>
2020-11-04 20:51:40 +0530

    footer done

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing", {
+        strings: ["FrontEnd Developer", "Full Stack Developer", "Web Developer", "Problem Solver", "Software Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["FrontEnd Developer", "Full Stack Developer", "Web Developer", "Problem Solver", "Software Developer"],
+        typeSpeed: 120,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
